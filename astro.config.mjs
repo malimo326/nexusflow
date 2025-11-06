b988031a6af9575260a2b723f2089259a5da09ae
malimo326 <mahmoodali2965.ma@gmail.com>
2025-11-06 12:57:18 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  site: 'https://nourhomsi.net',
-  base: '/nexusflow/',
+  site: "https://nourhomsi.net",
+  base: "/nexusflow/",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
